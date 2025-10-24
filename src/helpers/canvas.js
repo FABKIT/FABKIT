60b9a4cb0fd6cd500d8faf56edaed1f50a278cdb
TheDarkGlobe <lambstream@gmail.com>
2025-10-24 16:45:05 +0200

    Add touchmove support for pinch-to-zoom and pan gestures
    Implemented a new `initTouchMove` method to handle multi-touch gestures like pinch-to-zoom and panning. This ensures proper touch event handling, even during node dragging, by overriding default behaviors in the Konva library.

--- src/helpers/canvas.js
+++ src/helpers/canvas.js
@@ -48,6 +48,107 @@ const CanvasHelpers = class CanvasHelper {
         });
     }
 
+    initTouchMove() {
+      // by default Konva prevent some events when node is dragging
+      // it improve the performance and work well for 95% of cases
+      // we need to enable all events on Konva, even when we are dragging a node
+      // so it triggers touchmove correctly
+      Konva.hitOnDragEnabled = true;
+      const touchMoveLayer = this.stageLayer;
+      if (!touchMoveLayer) return;
+      const artworkLayer = this.artworkLayer;
+      if (!artworkLayer) return;
+
+      let lastCenter = null;
+      let lastDist = 0;
+      let dragStopped = false;
+
+      function getDistance(p1, p2) {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+      }
+
+      function getCenter(p1, p2) {
+        return {
+          x: (p1.x + p2.x) / 2,
+          y: (p1.y + p2.y) / 2,
+        };
+      }
+
+      touchMoveLayer.on('touchmove', function (e) {
+        e.evt.preventDefault();
+        const touch1 = e.evt.touches[0];
+        const touch2 = e.evt.touches[1];
+
+        // we need to restore dragging, if it was cancelled by multi-touch
+        if (touch1 && !touch2 && !touchMoveLayer.isDragging() && dragStopped) {
+          touchMoveLayer.startDrag();
+          dragStopped = false;
+        }
+
+        if (touch1 && touch2) {
+          // if the stage was under Konva's drag&drop
+          // we need to stop it, and implement our own pan logic with two pointers
+          if (touchMoveLayer.isDragging()) {
+            dragStopped = true;
+            touchMoveLayer.stopDrag();
+          }
+
+          const rect = touchMoveLayer.container().getBoundingClientRect();
+
+          const p1 = {
+            x: touch1.clientX - rect.left,
+            y: touch1.clientY - rect.top,
+          };
+          const p2 = {
+            x: touch2.clientX - rect.left,
+            y: touch2.clientY - rect.top,
+          };
+
+          if (!lastCenter) {
+            lastCenter = getCenter(p1, p2);
+            return;
+          }
+          const newCenter = getCenter(p1, p2);
+
+          const dist = getDistance(p1, p2);
+
+          if (!lastDist) {
+            lastDist = dist;
+          }
+
+          // local coordinates of center point
+          const pointTo = {
+            x: (newCenter.x - touchMoveLayer.x()) / touchMoveLayer.scaleX(),
+            y: (newCenter.y - touchMoveLayer.y()) / touchMoveLayer.scaleX(),
+          };
+
+          const scale = artworkLayer.scaleX() * (dist / lastDist);
+
+          artworkLayer.scaleX(scale);
+          artworkLayer.scaleY(scale);
+
+          // calculate new position of the stage
+          const dx = newCenter.x - lastCenter.x;
+          const dy = newCenter.y - lastCenter.y;
+
+          const newPos = {
+            x: newCenter.x - pointTo.x * scale + dx,
+            y: newCenter.y - pointTo.y * scale + dy,
+          };
+
+          artworkLayer.position(newPos);
+
+          lastDist = dist;
+          lastCenter = newCenter;
+        }
+      });
+
+      touchMoveLayer.on('touchend', function () {
+        lastDist = 0;
+        lastCenter = null;
+      });
+    }
+
     fitArtToCanvas(image){
         const bgW = this.backgroundLayer.width();
         const bgH = this.backgroundLayer.height();
@@ -99,6 +200,7 @@ const CanvasHelpers = class CanvasHelper {
                 self.artworkLayer.position({ x: 0, y: 0 });
 
                 self.artworkLayer.draggable(true);
+                self.initTouchMove();
                 self.initWheelResize();
             }
         );
