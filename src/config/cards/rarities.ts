2f4af302bf6351b0c5a0b3cd86ca6caac691154e
TheDarkGlobe <lambstream@gmail.com>
2026-02-10 22:16:15 +0100

    fix: rank the rarities by rank

--- src/config/cards/rarities.ts
+++ src/config/cards/rarities.ts
@@ -7,26 +7,6 @@ export const CardRarities = {
 		label: "card.rarity.common",
 		icon: "/img/rarities/rarity_common.svg",
 	},
-	fabled: {
-		label: "card.rarity.fabled",
-		icon: "/img/rarities/rarity_fabled.svg",
-	},
-	legendary: {
-		label: "card.rarity.legendary",
-		icon: "/img/rarities/rarity_legendary.svg",
-	},
-	majestic: {
-		label: "card.rarity.majestic",
-		icon: "/img/rarities/rarity_majestic.svg",
-	},
-	marvel: {
-		label: "card.rarity.marvel",
-		icon: "/img/rarities/rarity_marvel.svg",
-	},
-	promo: {
-		label: "card.rarity.promo",
-		icon: "/img/rarities/rarity_promo.svg",
-	},
 	rare: {
 		label: "card.rarity.rare",
 		icon: "/img/rarities/rarity_rare.svg",
@@ -35,9 +15,29 @@ export const CardRarities = {
 		label: "card.rarity.superrare",
 		icon: "/img/rarities/rarity_superrare.svg",
 	},
+	majestic: {
+		label: "card.rarity.majestic",
+		icon: "/img/rarities/rarity_majestic.svg",
+	},
+	legendary: {
+		label: "card.rarity.legendary",
+		icon: "/img/rarities/rarity_legendary.svg",
+	},
+	fabled: {
+		label: "card.rarity.fabled",
+		icon: "/img/rarities/rarity_fabled.svg",
+	},
 	token: {
 		label: "card.rarity.token",
 		icon: "/img/rarities/rarity_token.svg",
+	},
+	promo: {
+		label: "card.rarity.promo",
+		icon: "/img/rarities/rarity_promo.svg",
+	},
+	marvel: {
+		label: "card.rarity.marvel",
+		icon: "/img/rarities/rarity_marvel.svg",
 	},
 } as const;
 
